9475f339fab9befe771cab0ebcfbafc62b0f5734
afc163 <afc163@gmail.com>
2017-08-17 17:59:49 +0800

    Try create-react-class

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,10 @@
 import React from 'react'
-import { Table } from 'antd'
+import createReactClass from 'create-react-class'
 
-const dataSource = [{
-  key: '1',
-  name: '胡彦斌',
-  age: 32,
-  address: '西湖区湖底公园1号'
-}, {
-  key: '2',
-  name: '胡彦祖',
-  age: 42,
-  address: '西湖区湖底公园1号'
-}]
-
-const columns = [{
-  title: '姓名',
-  dataIndex: 'name',
-  key: 'name',
-}, {
-  title: '年龄',
-  dataIndex: 'age',
-  key: 'age',
-}, {
-  title: '住址',
-  dataIndex: 'address',
-  key: 'address',
-}]
-
-const App = () => (
-  <div className="test">
-    <Table dataSource={dataSource} columns={columns} />
-  </div>
-)
+const App = createReactClass({
+  render() {
+    return <div>123</div>
+  },
+})
 
 export default App
